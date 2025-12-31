48b55ce6d57c392a725b68747bd9fbce6e2abffb
AkhileshMishra <akhidaone.2008@gmail.com>
2025-12-31 15:08:28 +0800

    Update healthlake.tf

--- infra/healthlake.tf
+++ infra/healthlake.tf
@@ -1,7 +1,8 @@
 resource "awscc_healthlake_fhir_datastore" "store" {
   datastore_name         = "healthtech-store-${var.env}"
   datastore_type_version = "R4"
-  preload_data_config {
-    preload_data_type = "SYNTHEA" 
+
+  preload_data_config = {
+    preload_data_type = "SYNTHEA"
   }
 }
